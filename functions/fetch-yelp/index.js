67df650e630035af133cc0a73ddcdb387748bc39
olivia pasion <oap9820@gmail.com>
2022-10-11 15:36:05 -0700

    state updates properly and display reflects filters

--- functions/fetch-yelp/index.js
+++ functions/fetch-yelp/index.js
@@ -5,9 +5,11 @@ const handler = async (event) => {
   // add code here to fetch data from yelp API
   // be sure to include the parameters from event.queryStringParameters
   const zip = event.queryStringParameters.zip;
+  const search = event.queryStringParameters.search;
+
   try {
     const resp = await fetch(
-      `https://api.yelp.com/v3/businesses/search?categories=restaurants&location=${zip}`,
+      `https://api.yelp.com/v3/businesses/search?categories=restaurants&location=${zip}&term=${search}`,
       {
         headers: {
           Authorization: `Bearer ${process.env.REACT_APP_YELP_API_KEY}`,
@@ -15,7 +17,6 @@ const handler = async (event) => {
       }
     );
     const data = await resp.json();
-    console.log(data);
     return {
       statusCode: 200,
       body: JSON.stringify(data.businesses),
--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
   const [loading, setLoading] = useState(true);
 
   // TODO -- add state for zip / search and add event listeners to the inputs
-  const [restaurants, setRestaurants] = useState([]);
+  const [search, setSearch] = useState('');
   const [zip, setZip] = useState('93117');
 
   useEffect(() => {
@@ -16,15 +16,18 @@ function App() {
       const data = await fetchBusinesses();
       setBusinesses(data);
       setLoading(false);
+
     };
     fetchData();
   }, []);
   
   const searchRestaurants = async () => {
-    const resp = await fetch(`/.netlify/functions/yelp?zip=${zip}`);
-    const data = await resp.json();
-    setRestaurants(data);
+    const data = await fetchBusinesses(zip, search);
+    setBusinesses(data);
+    setLoading(false);
   };
+
+
 
   // TODO -- add event for button click to handle calling fetchBusinesses with zip / search
 
@@ -34,16 +37,13 @@ function App() {
       <div className="query-form">
         <div className="form-control">
           <label>Zip:</label>
-          <input type="text" placeholder="zip" value={zip} onChange={(e) => setZip(e.target.value)} />
+          <input type="text" placeholder="zip" onChange={(e) => setZip(e.target.value)} />
         </div>
         <div className="form-control">
           <label>Query:</label>
-          <input type="text" placeholder="Search..." />
+          <input type="text" placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
         </div>
         <button onClick={searchRestaurants}>Search</button>
-        {restaurants.map((restaurant) => (
-          <div key={restaurant.id}>{restaurant.name}</div>
-        ))}
       </div>
       {loading && <div className="loader"></div>}
       {!loading && businesses.map((b) => <RestaurantListItem key={b.id} {...b} />)}
